8aaac068c6d331c9bb2a5377c769b5176e6c9b0e
Gabriel Ribeiro <gabriel.hsribeiro19@gmail.com>
2024-04-17 22:21:52 -0300

    feat: add component skeleton and libs react query, axios

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,14 +1,24 @@
-import { Link } from 'react-router-dom'
 import { Header } from '../../components/common/Header'
 import { Grid } from '../../components/layout/Grid'
 import styles from './Pets.module.css'
+import { Card } from '../../components/common/Card'
+import { imageBase64 } from './mock'
+import { useEffect } from 'react'
+import { Skeleton } from '../../components/common/Skeleton'
 
 export function Pets() {
+  useEffect(() => {}, [])
   return (
     <Grid>
       <div className={styles.container}>
         <Header />
-        <Link to="/pets/20">Ir para a listagem</Link>
+        <main className={styles.list}>
+          {true && <Skeleton count={1} containerClassName={styles.skeleton} />}
+          <Card href="/pets/1" text="nina" thumb={imageBase64} />
+          <Card href="/pets/2" text="bob" thumb="https://placehold.co/400" />
+          <Card href="/pets/3" text="max" thumb="https://placehold.co/400" />
+          <Card href="/pets/3" text="max" thumb="https://placehold.co/400" />
+        </main>
       </div>
     </Grid>
   )
---
+++ src/services/pets/getPets.ts
@@ -0,0 +1,14 @@
+import { GetPetsRequest, GetPetsResponse } from '../../interfaces/pet'
+import httpClient from '../api/httpClient'
+
+export async function getPets(
+  params?: GetPetsRequest,
+): Promise<GetPetsResponse> {
+  try {
+    const { data } = await httpClient.get('/pet', { params })
+    return data
+  } catch (error) {
+    console.log('Erro ao buscar pets: ', error)
+    throw error
+  }
+}
